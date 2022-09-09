3d75f4ac19aabbe6e32b6443be21654ff4c20f5d
Samuel Luis dos Santos <mucasantos@gmail.com>
2022-09-09 15:33:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,15 @@ const routes = require("./routes");
 
 const server = http.createServer(routes);
 
+const database = require('./db');
+
+try {
+    const resultado =  database.sync();
+    
+    console.log(resultado);
+    
+} catch (error) {
+    console.log(error);
+}
+
 server.listen(3000);
