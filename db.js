e1c6d2f908805f5bc26775a79230c66d1e97653a
Samuel Luis dos Santos <mucasantos@gmail.com>
2022-09-27 10:54:14 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('samuca', 'root', 'usbw', {dialect: 'mysql', host: 'localhost'});
- 
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("aulas", "root", "usbw", {
+  dialect: "mysql",
+  host: "localhost",
+  port: 3307,
+});
+
 module.exports = sequelize;
